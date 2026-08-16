588ea2c728bd2a149a8c72cc0954e7046bfbe5bb
Avoy Sasmal <avoy-sasmal@outlook.com>
2026-08-17 02:12:30 +0530

    make some changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
+import CanvasTextDemo from "@/components/canvas-text-demo";
 import FocusCardsDemo from "@/components/focus-cards-demo";
 import TimelineDemo from "@/components/timeline-demo";
 
 export default function AboutPage() {
   return (
     <main className="overflow-x-hidden">
+      <CanvasTextDemo/>
       <FocusCardsDemo />
       <TimelineDemo />
     </main>
