a66eff1b47eb5f10e8f5f1e16aeaba6de8b86854
Avoy Sasmal <avoy-sasmal@outlook.com>
2026-08-17 02:13:05 +0530

    chnage in about

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,9 +1,13 @@
 import HeroParallaxDemo from "@/components/hero-parallax-demo";
+import FocusCardsDemo from "@/components/focus-cards-demo";
+import Cards from "@/blocks/interface-crafts-cards";
 
 export default function EventsPage() {
   return (
     <main className="section-light overflow-hidden">
       <HeroParallaxDemo />
+      <FocusCardsDemo />
+      <Cards />
     </main>
   );
 }
