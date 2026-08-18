508e8d1a335bff01a16e2160c3b026652729c7fa
Avoy Sasmal <avoy-sasmal@outlook.com>
2026-08-18 15:31:42 +0530

    changes in some layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,40 @@ import InfiniteMovingCardsDemo from "@/components/infinite-moving-cards-demo";
 import LinkPreviewDemo from "@/components/link-preview-demo";
 import MacbookScrollDemo from "@/components/macbook-scroll-demo";
 import AppleCardsCarouselDemo from "@/components/apple-cards-carousel-demo";
+import TimelineDemo from "@/components/timeline-demo";
+import AnimatedTestimonialsDemo from "@/components/animated-testimonials-demo";
+
 
 export default function HomePage() {
   return (
     <main className="overflow-x-hidden">
       {/* Cinematic hero */}
       <ImagesSliderDemo />
-      <CanvasTextDemo/>
-      <MacbookScrollDemo />
+      <AppleCardsCarouselDemo/>
+      <div className="flex flex-col md:flex-row w-full min-h-screen border-2 border-gray-800">
+  <div className="w-full md:w-1/2 border-b-2 md:border-b-0 md:border-r-2 border-gray-800 flex items-center justify-center">
+    <TimelineDemo />
+  </div>
+
+  <div className="w-full md:w-1/2 flex items-center justify-center">
+  <MacbookScrollDemo />
+  </div>
+</div>     
+     <CoverDemo />
       {/* Light reading band */}
       <LinkPreviewDemo />
+      <div className="flex flex-col md:flex-row w-full min-h-screen border-2 border-gray-800">
+  <div className="w-full md:w-1/2 border-b-2 md:border-b-0 md:border-r-2 border-gray-800 flex items-center justify-center">
+    <AnimatedTestimonialsDemo/>
+  </div>
+
+  <div className="w-full md:w-1/2 flex items-center justify-center">
+  <InfiniteMovingCardsDemo />
+  </div>
+</div>
       {/* Dark showcase band */}
-      <InfiniteMovingCardsDemo />
-      <AppleCardsCarouselDemo/>
       {/* Dark showcase band */}
-      <CoverDemo />
+ <CanvasTextDemo/>
     </main>
   );
 }
